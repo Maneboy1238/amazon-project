14ecacfd9a7660f296ca52b0a1c7e0b8ffbb9eab
maneboy <okonudumanasseh@gmail.com>
2025-10-23 17:11:04 +0000

    converted our product objects to classes and also changed our ratings image to be called by a method in the class and our price to be called by a method in the class

--- data/products.js
+++ data/products.js
@@ -1,6 +1,27 @@
+import formatCurrency from "../scripts/utils/utility";
 export function getProduct (productId) {
   let matchingProduct = products.find(product => product.id === productId)
   return matchingProduct
+}
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+  getStarUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+  getPrice() {
+    return formatCurrency(this.priceCents)
+  }
 }
 export const products = [
   {
@@ -661,4 +682,9 @@ export const products = [
       "mens"
     ]
   }
-];
+].map(
+  product => {
+    return new Product(product) 
+  }
+);
+console.log(products);
