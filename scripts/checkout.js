f671fb0d338f2954b3321c148f2cb90b0da20f2f
maneboy <okonudumanasseh@gmail.com>
2025-11-13 22:05:53 +0000

    we used promises in our code instead of callbacks

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,9 +2,46 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkout-header.js";
 import { loadProducts } from "../data/products.js";
-import "../data/backend-practice.js";
-loadProducts(()=> {
+import { loadCart } from "../data/cart.js";
+//import "../data/backend-practice.js";
+Promise.all([
+new Promise((resolve)=> {
+    loadProducts(()=> {
+        resolve()
+    })
+}),
+new Promise((resolve)=> {
+        loadCart(()=> {
+            resolve();
+        })
+    })
+]).then(()=> {
 renderCheckoutHeader();
-renderOrderSummary();
-renderPaymentSummary();
+    renderOrderSummary();
+    renderPaymentSummary();
 })
+/*
+new Promise((resolve)=> {
+    loadProducts(()=> {
+        resolve()
+    })
+}).then(()=> {
+    return new Promise((resolve)=> {
+        loadCart(()=> {
+            resolve();
+        })
+    })
+}).then(()=> {
+
+    
+});
+*/ 
+/*
+loadProducts(()=> {
+    loadCart(()=> {
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+    })
+})
+*/
